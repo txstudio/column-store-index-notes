716a63baf19de7f8684755ae07a15037699f011e
Shi Hao Wang <txstudio@outlook.com>
2018-12-22 15:40:18 +0800

    修正排版

--- init.sql
+++ init.sql
@@ -40,16 +40,16 @@ GO
 
 CREATE TABLE [dbo].[SalesOrderDetailFromAdventureWorks2017]
 (
-	[SalesOrderID]				INT					NOT NULL,
+	[SalesOrderID]			INT			NOT NULL,
 	[SalesOrderDetailID]		INT IDENTITY(1,1)	NOT NULL,
 	[CarrierTrackingNumber]		NVARCHAR(25)		NULL,
-	[OrderQty]					SMALLINT			NOT NULL,
-	[ProductID]					INT					NOT NULL,
-	[SpecialOfferID]			INT					NOT NULL,
-	[UnitPrice]					MONEY				NOT NULL,
-	[UnitPriceDiscount]			MONEY				NOT NULL,
-	[LineTotal]					NUMERIC(38, 6)		NOT NULL,
-	[rowguid]					UNIQUEIDENTIFIER	NOT NULL,
-	[ModifiedDate]				DATETIME			NOT NULL
+	[OrderQty]			SMALLINT		NOT NULL,
+	[ProductID]			INT			NOT NULL,
+	[SpecialOfferID]		INT			NOT NULL,
+	[UnitPrice]			MONEY			NOT NULL,
+	[UnitPriceDiscount]		MONEY			NOT NULL,
+	[LineTotal]			NUMERIC(38, 6)		NOT NULL,
+	[rowguid]			UNIQUEIDENTIFIER	NOT NULL,
+	[ModifiedDate]			DATETIME		NOT NULL
 )
 GO
